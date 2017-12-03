316ceef17bb783e3a4e2303e66c51aed2e5be0a1
Arcaz21 <arcaz.johnlns@gmail.com>
2017-12-03 17:21:48 +0800

    Added and updated some functions

--- Lula/views/tabs.js
+++ Lula/views/tabs.js
@@ -1,6 +1,9 @@
-window.onload = function(){
-  document.getElementById('autoclick').click();
-}
+// Logs:
+// -Remove window.onload function 'para dili sige balik sa id na gi set everyload'
+// -Added auto click on default sa end sa options finction
+// -Added getQueryStringVariable para capture ang URL. Added condition.
+//Changed redirect location after update button click.
+
 function options(evt, opName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -12,5 +15,32 @@ function options(evt, opName) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
     document.getElementById(opName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+document.getElementById('defaultOpen').click();
+
+//Caputre sa URL
+function getQueryStringVariable(variable)
+{
+   var queryString = window.location.search.substring(1);
+   var vars = queryString.split("&");
+   for (var i=0;i<vars.length;i++) {
+       var pair = vars[i].split("=");
+       if(pair[0] == variable){
+            return pair[1];
+        }
+   }
+   return false;
+}
+//condition para mag show either si students or Userlist after Update
+var view = getQueryStringVariable("view");
+if (view === "yes")
+{   
+    document.getElementById("Userslist").style.display = "block";
+    evt.currentTarget.className += " active";   
+}
+else
+{
+    document.getElementById("Students").style.display = "block";
     evt.currentTarget.className += " active";
 }
